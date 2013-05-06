15ba3386588f07cbe0228f0f8175ba597c08a988
Pedro Maia <maiarib@gmail.com>
2013-05-06 23:16:16 +0100

    Criadas Classes
    Classe do cone criada.

---
+++ ProjectoBAR/ProjectoBAR/Cone.cpp
@@ -0,0 +1,7 @@
+#define _USE_MATH_DEFINES
+#include "Cilindro.h"
+
+#include <stdlib.h>
+#include <GL/glew.h>
+#include <GL/glut.h>
+#include <math.h>
---
+++ ProjectoBAR/ProjectoBAR/Cone.h
@@ -0,0 +1,13 @@
+#pragma once
+class Cone
+{
+public:
+	Cone(void){};
+	Cone(float raio, float altura, int divh, int divc);
+	~Cone(void);
+	void desenhar();
+protected:
+	unsigned int buffers[2];
+	int count;
+	int *indices;
+};
